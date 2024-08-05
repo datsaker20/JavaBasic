b9130b440fcc75cd109774ce1a5aaad3993e1af6
Trần Viết Đạt <viet.dat.tran@draphony.com>
2024-08-05 14:43:39 +0700

    function reverse

--- src/Hello/HelloWorld.java
+++ src/Hello/HelloWorld.java
@@ -1,7 +1,20 @@
 package Hello;
 
 public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	public boolean isReverse(String s) {
+		String reverse = "";
+		for (int i = s.length() - 1; i >= 0; i--) {
+			reverse += s.charAt(i);
+		}
+		return reverse.equals(s);
 	}
+
+	public static void main(String[] args) {
+		HelloWorld obj = new HelloWorld();
+		String name = "haha";
+		String funcReverse = obj.isReverse(name) ? name + " Là kí tự đả ngược" : name + " Không là kí tự đảo ngược";
+		System.out.println(funcReverse);
+
+	}
+
 }
